e88746ac19f640571e82ad7b1ec0288f2bc6daf8
Ahmed-Ojo <ahmed_ojo1@yahoo.com>
2024-05-20 19:17:55 +0100

    Dev_3 code change

--- src/basic/HelloGitConflictExercise.java
+++ src/basic/HelloGitConflictExercise.java
@@ -5,6 +5,12 @@ public class HelloGitConflictExercise {
 	public static void main(String[] args) {
 		System.out.println("This is Git Conflict Exercise");
 
+
+		 // Creates a reader instance which takes
+        // input from standard input - keyboard
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+
 	}
 
 }
